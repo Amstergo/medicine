12bc4f8c2773e41e9bc6aa819f611041c496ca56
John <amstergo46@gmail.com>
2016-07-31 20:42:21 +0300

    db.setUpConnection

---
+++ server/utils/DataBaseUtils.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+import  '../model/Note';
+
+const Note = mongoose.model('Note');
+
+export function SetUpConnection() {
+  mongoose.connect('mongodb://localhost/notes');
+}
+
+export function listNotes() {
+  return Note.find();
+}
+
+export function createNote(data) {
+  const note = new Note( {
+    title: data,title,
+    text: data,text,
+    color: data,color,
+    createdAt: new Date()
+  });
+
+  return note.save();
+}
+
+export function deleteNote(id) {
+  return Note.findById(id).remove();
+
+}
